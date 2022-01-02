24551d5f950d820b2718282d0872b3976479ebdb
roberto.dibella096@gmail.com <s267517@po.polito.it>
2022-01-02 18:54:41 +0100

    Try for multiple clock signals generation

--- riscv/gate/syn_script.tcl
+++ riscv/gate/syn_script.tcl
@@ -6,7 +6,7 @@ set synthetic_library dw_foundation.sldb
 set target_library {../techlib/NangateOpenCellLibrary_typical_ccs_scan.db}
 set link_library [list $target_library $synthetic_library]
 #set link_library {../techlib/NangateOpenCellLibrary_typical_ccs_scan.db}
-read_db $target_library
+read_db ../techLib/NangateOpenCellLibrary_typical_ccs_scan.db
 
 set DFF_CELL DFF_X2
 set LIB_DFF_D NangateOpenCellLibrary/DFF_X2/D
@@ -16,8 +16,8 @@ set OPER_COND typical
 # VARIABLES for SCAN CONFIGURATION
 # Change it here
 
-set scanChains 10
-set scanCompress 10
+set scanChains 3
+set scanCompress 50
 
 ##############################################
 
@@ -44,10 +44,20 @@ read_ddc riscv_core.ddc
 # scan chains and clock ports?
 # - Do I have to set the scan chains manually AFTER the dft insertion?
 
+####################### TRY 1: MANUAL PORT INSERTION ##########################
+# 1. Manual port definition
+# 2. AFTER insert_dft, connect clocks to the different scan chains
+###############################################################################
+
+create_port -direction "in" {sclk1 sclk2 sclk3}
+create_clock sclk1
+create_clock sclk2
+create_clock sclk3
+
 # Compile ULTRA
 compile_ultra -incremental -gate_clock -scan -no_autoungroup
 
-set_dft_clock_gating_pin [get_cells * -hierarchical -filr "@ref_name =~ SNPS_CLOCK_GATE*"] -pin_name TE
+set_dft_clock_gating_pin [get_cells * -hierarchical -filter "@ref_name =~ SNPS_CLOCK_GATE*"] -pin_name TE
 
 report_area
 set_dft_configuration -scan_compression enable
@@ -55,7 +65,20 @@ set_dft_configuration -scan_compression enable
 set test_default_scan_style multiplexed_flip_flop
 
 set_dft_signal -view existing_dft -type ScanEnable -port test_en_i
-set_dft_signal -view spec -type ScaneEnable -port test_en_i
+set_dft_signal -view spec -type ScanEnable -port test_en_i
+
+###############################################################################
+# Scan Clock ports definition
+
+#set_dft_signal -view existing_dft -type ScanClock -port sclk1
+set_dft_signal -view spec -type ScanClock -port sclk1
+
+#set_dft_signal -view existing_dft -type ScanClock -port sclk2
+set_dft_signal -view spec -type ScanClock -port sclk2
+
+#set_dft_signal -view existing_dft -type ScanClock -port sclk3
+set_dft_signal -view spec -type ScanClock -port sclk3
+###############################################################################
 
 set_scan_configuration -chain_count $scanChains
 set_scan_compression_configuration -chain_count $scanCompress
