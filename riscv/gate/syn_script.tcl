83a2ea7e2e4c5264ae741380a1318fdfb5c85c10
roberto.dibella096@gmail.com <s267517@po.polito.it>
2022-01-06 01:33:41 +0100

    Optimized scan insertion scripts

--- riscv/gate/syn_script.tcl
+++ riscv/gate/syn_script.tcl
@@ -1,3 +1,18 @@
+##################### RI5CY - SCAN INSERTION ############################################
+# Script that inserts scan chains to the RI5CY gate-level synthesis.			#
+# 											#
+# Input files: 	riscv_core.v		RI5CY Gate-level description (provided)		#
+# 		library			NangateOpenCell Library				#
+# 											#
+# Output files:	riscv_scan.v		RI5CY gate-level with scan insertion		#
+#		riscv_scan.spf		STIL procedure file				#
+#########################################################################################
+
+############ TO-DO LIST #################################################
+# - Find the optimum scanChain/scanCompress ratio
+# - Understand if there are violations and how to eliminate them
+#########################################################################
+
 # Set of the libraries
 # source NangateBlablabla.dc_setup_synthesis.tcl
 
@@ -26,32 +41,39 @@ set scanCompress 20
 #current_design riscv_core_0_128_1_16_1_1_0_0_0_0_0_0_0_0_0_3_6_15_5_1a110800
 #link
 
-# From now on, the top-level is the entire core (name retrieved from netlist)
-#
-# Now, how to optimize?
+# Read DDC -> if the synthesis was already run.
+# In this case, it is chosen to do everything in the "classic" way
+read_ddc ../riscv_core.ddc 
 
-# Read DDC
-read_ddc riscv_core.ddc 
+# Create the clock -> the compiler knows that there is a clock signal
+#set CLOCK_SPEED 2
+#create_clock      [get_ports clk_i] -period $CLOCK_SPEED -name REF_CLK
 
 # Compile ULTRA
 compile_ultra -incremental -gate_clock -scan -no_autoungroup
 
+############################### Design For Testability configurations ########################################
+
 set_dft_clock_gating_pin [get_cells * -hierarchical -filter "@ref_name =~ SNPS_CLOCK_GATE*"] -pin_name TE
 
-report_area > area_pre_dft.txt
+report_area > ../rpt/area_pre_dft.txt
 set_dft_configuration -scan_compression enable
 
 set test_default_scan_style multiplexed_flip_flop
 
 set_dft_signal -view existing_dft -type ScanEnable -port test_en_i
-set_dft_signal -view spec -type ScaneEnable -port test_en_i
+set_dft_signal -view spec -type ScanEnable -port test_en_i
+
+set_dft_configuration -testability enable
+
+set_scan_element false NangateOpenCellLibrary/DLH_X1
 
 set_scan_configuration -chain_count $scanChains
 set_scan_compression_configuration -chain_count $scanCompress
 
 create_test_protocol -infer_asynch -infer_clock
 dft_drc
-preview_dft
+preview_dft > ../rpt/scan_dft.txt
 insert_dft
 
 change_names -rules verilog -hierarchy
@@ -60,7 +82,8 @@ report_scan_path -test_mode all
 report_scan_path -view existing -chain all > chains.txt
 report_area > area_post_dft.txt
 
-write -hierarchy -format verilog -output "riscv_scan.v"
+write -hierarchy -format verilog -output "../riscv_scan.v"
+write_file -format ddc -hierarchy -output "../riscv_scan.ddc"
 write_sdf -version 3.0 "riscv_scan.sdf"
 write_sdc "riscv_scan.sdc"
 write_test_protocol -output "riscv_scan.spf" -test_mode Internal_scan
