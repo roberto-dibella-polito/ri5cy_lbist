8e2b45870f156710388d13345d8cd1e923933d9e
Roberto Di Bella <s267517@po.polito.it>
2022-01-11 16:55:40 +0100

    RISC-V LBIST almost completed

--- riscv/rtl/riscv_scan_core_lbist.vhd
+++ riscv/rtl/riscv_scan_core_lbist.vhd
@@ -107,6 +107,7 @@ begin
 		pi_selected	=> pis_selected_i
 		go_nogo		=> go_nogo_o	);
 	
+	-- POSITIONAL CONNECTION
 	-- Connect together all primary inputs into pis_i
 	
 	pis_i(31 downto 0) 	<= boot_addr_i;
@@ -117,14 +118,86 @@ begin
   	pis_i(234 downto 203)	<= apu_master_result_i;
   	pis_i(239 downto 235)	<= apu_master_flags_i;
   	pis_i(244 downto 240) 	<= irq_id_i;
-  	pis_i(245 to 247)	<= ext_perf_counters_i;
+  	pis_i(245 to 246)	<= ext_perf_counters_i;
 	pis_i(256 downto 247)	<= fregfile_disable_i & instr_gnt_i & instr_rvalid_i & data_gnt_i & data_rvalid_i & apu_master_gnt_i & apu_master_valid_i & irq_i & irq_sec_i & debug_req_i;
-	
-	-- This signals will enter the mux of the LBIST and eventually coming out in the same position from pis_selected
+	pis_i(263 downto 257)	<= (others=>'0'); -- Dummy ports for SCAN CHAINS port 
+
+	-- These signals will enter the mux of the LBIST and eventually coming out in the same position from pis_selected
 	--> Reconnect everything to the DUT
 
+	-- Same for POs: will be connected all to the same bus and then sent to output divided again
+	
+	core: riscv_core_0_128_1_16_1_1_0_0_0_0_0_0_0_0_0_3_6_15_5_1a110800 port map(
+			boot_addr_i 		=> pis_selected_i(31 downto 0),
+  			core_id_i		=> pis_selected_i(35 downto 32),
+  			cluster_id_i 		=> pis_selected_i(42 downto 36),	
+  			instr_addr_o		=> pos_i(31 downto 0),
+  			instr_rdata_i		=> pis_selected_i(170 downto 43),
+  			data_be_o		=> pos_i(35 downto 32),
+  			data_addr_o		=> pos_i(67 downto 36),
+  			data_wdata_o		=> pos_i(99 downto 68),
+  			data_rdata_i		=> pis_selected_i(202 downto 171),
+  			apu_master_operands_o	=> pos_i(195 downto 100),
+ 			apu_master_op_o		=> pos_i(201 downto 196),
+  			apu_master_type_o	=> pos_i(202 to 203),
+  			apu_master_flags_o	=> pos_i(218 downto 204),
+  			apu_master_result_i	=> pis_selected_i(234 downto 203),
+  			apu_master_flags_i	=> pis_selected_i(239 downto 235),
+  			irq_id_i		=> pis_selected_i(244 downto 240),
+  			irq_id_o		=> pos_i(223 downto 219);
+  			ext_perf_counters_i	=> pis_selected_i(245 to 246),
 
-	-- Connect together all primary outputs into pos_i
+			
+			-- Signals for the top-level entity
+ 	 		clk_i			=> clk_i, 
+			rst_ni			=> rst_ni, 
+			clock_en_i		=> clk_en_i, 
+
+			test_en_i		=> test_select, 
+			test_mode		=> '1', 
+			fetch_enable_i		=> fetch_enable_i,
+			
+			-- 1-bit primary inputs
+			fregfile_disable_i	=> pis_selected_i(256), 
+			instr_gnt_i		=> pis_selected_i(255), 			
+			instr_rvalid_i		=> pis_selected_i(254),
+			data_gnt_i		=> pis_selected_i(253),
+			data_rvalid_i		=> pis_selected_i(252),
+			apu_master_gnt_i	=> pis_selected_i(251),	
+			apu_master_valid_i	=> pis_selected_i(250),
+			irq_i			=> pis_selected_i(249),
+			irq_sec_i		=> pis_selected_i(248),
+			debug_req_i		=> pis_selected_i(247),
+         		
+			-- Scan input port -> MSBs of the selected pis
+			test_si1		=> pis_selected_i(257), 
+			test_si2		=> pis_selected_i(258), 
+			test_si3		=> pis_selected_i(259), 
+			test_si4		=> pis_selected_i(260), 
+			test_si5		=> pis_selected_i(261), 
+			test_si6		=> pis_selected_i(262), 
+			test_si7		=> pis_selected_i(263),
+         		
+			-- 1-bit primary outputs
+	 		instr_req_o		=> pos_i(224), 
+			data_req_o		=> pos_i(225),
+			data_we_o		=> pos_i(226),
+			apu_master_req_o	=> pos_i(227),
+         		apu_master_ready_o	=> pos_i(228),
+			irq_ack_o		=> pos_i(229),
+			sec_lvl_o		=> pos_i(230),
+			core_busy_o		=> pos_i(231),
+			
+			-- Output scan ports						
+			test_so1		=> pos_i(232),
+			test_so2 		=> pos_i(233),
+			test_so3 		=> pos_i(234),
+			test_so4 		=> pos_i(235),
+			test_so5 		=> pos_i(236),
+			test_so6 		=> pos_i(237),
+			test_so7		=> pos_i(238)
+		);
+	-- Connect together all primary outputs into pos_i AND to the output ports
 	
 	
 	-- connect the selected_pis in the same way of the input + add the scan signals
