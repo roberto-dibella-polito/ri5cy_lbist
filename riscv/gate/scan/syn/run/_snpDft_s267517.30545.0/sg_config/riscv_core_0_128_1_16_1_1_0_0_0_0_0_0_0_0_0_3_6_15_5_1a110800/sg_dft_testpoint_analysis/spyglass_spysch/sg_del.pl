bce8a032fe1ac5e65bb542002fbcbc0ac4d4528a
Roberto Di Bella <s267517@po.polito.it>
2022-01-13 17:46:06 +0100

    TMAX simulation with test insertion point still fails

---
+++ riscv/gate/scan/syn/run/_snpDft_s267517.30545.0/sg_config/riscv_core_0_128_1_16_1_1_0_0_0_0_0_0_0_0_0_3_6_15_5_1a110800/sg_dft_testpoint_analysis/spyglass_spysch/sg_del.pl
@@ -0,0 +1,5 @@
+use spyglass;
+use SpyGlass;
+use SpyGlass::Objects;
+&spyGenerateDelViolHash("./sg_config/riscv_core_0_128_1_16_1_1_0_0_0_0_0_0_0_0_0_3_6_15_5_1a110800/sg_dft_testpoint_analysis/spyglass_spysch/sg_msgtag.txt");
+1;
