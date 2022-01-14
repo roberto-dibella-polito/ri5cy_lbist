4aea218d3cde9e2b58e3fc87cf3392311828ab95
Roberto Di Bella <s267517@po.polito.it>
2022-01-14 12:29:52 +0100

    Adjusted synthesis and fault simulation scripts

--- riscv/gate/scan/test/riscv_atpg_scancompress.tcl
+++ riscv/gate/scan/test/riscv_atpg_scancompress.tcl
@@ -4,7 +4,7 @@
 #########################################################
 
 read_netlist ../../syn/techlib/NangateOpenCellLibrary.v -library -insensitive
-read_netlist ../../syn/output/riscv_core_scan.v -master -insensitive
+read_netlist ../../syn/output/riscv_core_scan.v -master
 run_build_model riscv_core_0_128_1_16_1_1_0_0_0_0_0_0_0_0_0_3_6_15_5_1a110800
 
 run_drc ../../syn/output/riscv_core_scancompress.spf
